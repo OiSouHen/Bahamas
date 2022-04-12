fe5f653238956fe807b4e6e56ab3e39198907d7c
Lucas Hen <oisouhen@gmail.com>
2022-04-12 14:31:17 -0300

    Update build

--- server/server.bat
+++ server/server.bat
@@ -1,1 +1,1 @@
-..\artifacts\FXServer.exe +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_forceMigration 1 +set onesync_distanceCullVehicles 1 +set sv_enforceGameBuild 2189 +exec server.cfg
+..\artifacts\FXServer.exe +set onesync on +set onesync_enableInfinity 1 +set onesync_enableBeyond 1 +set onesync_forceMigration 1 +set onesync_distanceCullVehicles 1 +set sv_enforceGameBuild 2545 +exec server.cfg
