e286d03471db885ee82f78283d49b91d1e08a2c2
Lucas Hen <oisouhen@gmail.com>
2022-05-03 12:42:21 -0300

    Update server.lua

--- server/resources/[system]/admin/server-side/server.lua
+++ server/resources/[system]/admin/server-side/server.lua
@@ -290,10 +290,10 @@ end)
 RegisterCommand("group",function(source,args,rawCommand)
 	local user_id = vRP.getUserId(source)
 	if user_id then
-	--	if vRP.hasGroup(user_id,"Moderador") and parseInt(args[1]) > 0 and args[2] then -- Need remove to set the First server owner rank
+		if vRP.hasGroup(user_id,"Admin") or user_id == 1 and parseInt(args[1]) > 0 and args[2] then
 			TriggerClientEvent("Notify",source,"verde","Adicionado <b>"..args[2].."</b> ao passaporte <b>"..args[1].."</b>.",5000)
 			vRP.setPermission(args[1],args[2])
-	--	end -- Need remove to set the First server owner rank
+		end
 	end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
